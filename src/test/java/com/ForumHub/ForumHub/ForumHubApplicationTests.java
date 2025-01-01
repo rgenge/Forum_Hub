af873706e4bec35d8deb896c2585f7c06bcfc3d1
Atila <chaves_87@hotmail.com>
2025-01-01 18:00:50 -0400

    feat: First commit

---
+++ src/test/java/com/ForumHub/ForumHub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ForumHub.ForumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
